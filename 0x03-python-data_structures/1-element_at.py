3027e05c3de4e2d08d8286b4d0a0df360d8236a5
mbawuike3 <mbawuikestanley@gmail.com>
2022-06-05 07:36:09 -0700

    checking for an index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return (none)
+    length = len(my_list)
+    if idx > length - 1:
+        return (none)
+    return (my_list[idx])
