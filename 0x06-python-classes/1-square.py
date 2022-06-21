a19e71069d07e399143d0901cfd67657912b1c42
mbawuike3 <mbawuikestanley@gmail.com>
2022-06-21 14:38:53 -0700

    fixed bug

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-class Square:
-    """Represent a square."""
 
+class Square:
     def __init__(self, size):
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new square.
-        """
         self.__size = size
