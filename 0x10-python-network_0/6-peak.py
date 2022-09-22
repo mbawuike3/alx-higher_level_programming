62f7c23d3e0a93402841b409699dab27f3173882
mbawuike3 <mbawuikestanley@gmail.com>
2022-09-22 15:42:18 -0700

    time complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
-"""contains the function find_peak"""
+"""Find a peak in a list of unsorted integers algorithm"""
 
 
 def find_peak(list_of_integers):
-    """finds a peak in a list of unsorted integers"""
-    li = list_of_integers
-    l = len(li)
-    if l == 0:
-        return
-    m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
-        return li[m]
-    if m != l - 1 and li[m + 1] > li[m]:
-        return find_peak(li[m + 1:])
-    return find_peak(li[:m])
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    length = len(list_of_integers)
+
+    if length == 1:
+        return list_of_integers[0]
+    elif length == 2:
+        return max(list_of_integers)
+
+    mid = length // 2
+    peak = list_of_integers[mid]
+
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
+
+
+if __name__ == '__main__':
+    print(find_peak([1, 2, 4, 6, 3]))
+    print(find_peak([4, 2, 1, 2, 3, 1]))
+    print(find_peak([2, 2, 2]))
+    print(find_peak([]))
+    print(find_peak([-2, -4, 2, 1]))
+    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
