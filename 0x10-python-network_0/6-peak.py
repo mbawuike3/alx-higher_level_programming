7140f2c37701da407655d52fb3a8124c1fc7195e
mbawuike3 <mbawuikestanley@gmail.com>
2022-09-22 15:47:32 -0700

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python3
-"""Find a peak in a list of unsorted integers algorithm"""
+"""task 6 modele"""
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers."""
+    """Return a peak in a list of unsorted integers."""
     if list_of_integers == []:
         return None
 
-    length = len(list_of_integers)
-
-    if length == 1:
+    size = len(list_of_integers)
+    if size == 1:
         return list_of_integers[0]
-    elif length == 2:
+    elif size == 2:
         return max(list_of_integers)
 
-    mid = length // 2
+    mid = int(size / 2)
     peak = list_of_integers[mid]
-
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
     elif peak < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
     else:
         return find_peak(list_of_integers[mid + 1:])
-
-
-if __name__ == '__main__':
-    print(find_peak([1, 2, 4, 6, 3]))
-    print(find_peak([4, 2, 1, 2, 3, 1]))
-    print(find_peak([2, 2, 2]))
-    print(find_peak([]))
-    print(find_peak([-2, -4, 2, 1]))
-    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
