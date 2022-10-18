093bc5b683614f612b022b852d97e777195bf04f
Mbawuike Stanley <mbawuikestanley@gmail.com>
2022-10-18 22:03:33 +0000

    fixing bugs

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,20 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
-  }
+
+request(process.argv[2], (err, res, body) => {
+  if (err) console.log(err);
+
+  const todos = JSON.parse(body);
+  const data = {};
+
+  todos.forEach((todo) => {
+    if (todo.completed && data[todo.userId] === undefined) {
+      data[todo.userId] = 1;
+    } else if (todo.completed) {
+      data[todo.userId] += 1;
+    }
+  });
+
+  console.log(data);
 });
